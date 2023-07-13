4350f8a8242a511b601f31925a700770592ac03d
Maximum0706 <128177758+Maximum0706@users.noreply.github.com>
2023-07-13 17:40:20 +0700

    Add files via upload

---
+++ Maximumconvert/container.php
@@ -0,0 +1,23 @@
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="http://localhost/SSIP/maximumweb/" class="navbar-brand">Maximum Convert Currency</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://localhost/SSIP/maximumweb/">Home</a></li>
+           
+          </ul>
+         
+        </div>
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
